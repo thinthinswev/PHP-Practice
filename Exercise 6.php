76390d97394f00ae6074a5cf33f3b98dc53cda83
thinthinswev <169077520+thinthinswev@users.noreply.github.com>
2024-05-11 14:34:53 +0630

    Add files via upload

---
+++ Exercise 6.php
@@ -0,0 +1,24 @@
+ <?php
+
+function three_Sum_zero($arr)
+{
+    $count = count($arr) - 2;
+    $result=[];
+    for ($x = 0; $x < $count; $x++) 
+    {
+        if ($arr[$x] + $arr[$x+1] + $arr[$x+2] == 0)
+        {
+            
+            array_push($result, "{$arr[$x]} + {$arr[$x+1]} + {$arr[$x+2]} = 0");
+        }
+    }
+   
+    return $result;
+}
+
+
+$nums1= array(-1,0,1,2,-1,-4);
+$nums2 = array(-25,-10,-7,-3,2,4,8,10);
+print_r(three_Sum_zero($nums1));
+print_r(three_Sum_zero($nums2));
+?>
